f5cbc8aab8ed9b4daec1d60e73f7f66059803476
Luca Weiss <luca@z3ntu.xyz>
2019-07-19 13:43:04 +0200

    libusb: iconv on NetBSD wants a const char*

--- libusb/hid.c
+++ libusb/hid.c
@@ -397,7 +397,11 @@ static wchar_t *get_usb_string(libusb_device_handle *dev, uint8_t idx)
 	size_t inbytes;
 	size_t outbytes;
 	size_t res;
+#ifdef __NetBSD__
+	const char *inptr;
+#else
 	char *inptr;
+#endif
 	char *outptr;
 #endif
 
