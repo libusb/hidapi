9f75d4edf0e6b02931aabb63fc2550461e9f2892
Daniel Van Noord <sg1mash98@gmail.com>
2019-08-23 19:01:08 -0400

    Removed incorrect Linux implementation.

--- linux/hid.c
+++ linux/hid.c
@@ -791,24 +791,10 @@ int HID_API_EXPORT hid_get_feature_report(hid_device *dev, unsigned char *data, 
 	return res;
 }
 
+//Not implemented for Linux HidRaw yet
 int HID_API_EXPORT HID_API_CALL hid_get_input_report(hid_device *dev, unsigned char *data, size_t length)
 {
-	int res;
-	unsigned char bfr[64] = {0}; //Maximum Hid Report Size is 64 bytes, added one extra. See https://www.usb.org/sites/default/files/documents/hid1_11.pdf section 5.6
-
-	if(length > 64){
-		register_device_error_format(dev, "ioctl (GREPORT): %s", strerror(EINVAL)); //22      /* Invalid argument */
-	}
-
-	memcpy(bfr, data, length); // copy the data onto the buffer	
-
-	res = ioctl(dev->device_handle, HIDIOCGREPORT, bfr);
-	if (res < 0)
-		register_device_error_format(dev, "ioctl (GREPORT): %s", strerror(errno));
-
-	memcpy(data, bfr, length);	
-
-	return res;
+	return -1;
 }
 
 void HID_API_EXPORT hid_close(hid_device *dev)
