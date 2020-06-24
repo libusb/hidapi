1e74e60334938993ce0f7b0b877e928dadc4a197
Lennie Araki <laraki@thermal.com>
2020-06-24 15:44:42 -0700

    Generalize seekto.bat

--- windows/seekto.bat
+++ windows/seekto.bat
@@ -1,1 +1,7 @@
-Debug\hidtest.exe -v289d -p1000 -o00,0102,8000,8000,0000,0000,0000,0000,0000 -l15
+if .%1==. goto default
+set POS=%1
+goto endif
+:default
+set POS=8000
+:endif
+Debug\hidtest.exe -v289d -p1000 -o00,0102,%POS%,%POS%,0000,0000,0000,0000,0000 -l15
