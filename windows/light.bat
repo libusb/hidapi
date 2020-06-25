7de15068d0cb72af7820c3f1c10b5aa3fe862a38
Lennie Araki <laraki@thermal.com>
2020-06-25 09:59:23 -0700

    Change .bat files to work with either Debug\hidtest.exe or hidtest.exe

--- windows/light.bat
+++ windows/light.bat
@@ -1,7 +1,13 @@
+if exist Debug\hidtest.exe goto debug
+set HIDTEST=hidtest.exe
+goto check
+:debug
+set HIDTEST=Debug\hidtest.exe
+:check
 if .%1==. goto default
 set LIGHT=%1
 goto endif
 :default
 set LIGHT=0002
 :endif
-Debug\hidtest.exe -v289d -p1000 -o00,0001,%LIGHT%,0000,0000,0000,0000,0000,0000 -l15
+%HIDTEST% -v289d -p1000 -o00,0001,%LIGHT%,0000,0000,0000,0000,0000,0000 -l15
--- windows/stepto.bat
+++ windows/stepto.bat
@@ -1,7 +1,13 @@
+if exist Debug\hidtest.exe goto debug
+set HIDTEST=hidtest.exe
+goto check
+:debug
+set HIDTEST=Debug\hidtest.exe
+:check
 if .%1==. goto default
 set POS=%1
 goto endif
 :default
 set POS=8000
 :endif
-Debug\hidtest.exe -v289d -p1000 -o00,0102,%POS%,%POS%,0000,0000,0000,0000,0000 -l15
+%HIDTEST% -v289d -p1000 -o00,0102,%POS%,%POS%,0000,0000,0000,0000,0000 -l15
