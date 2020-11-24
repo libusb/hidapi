f6d0073fcddbdda24549199445e844971d3c9cef
Ihor Dutchak <ihor.youw@gmail.com>
2020-11-24 13:39:27 +0200

    hidraw/libusb: fix -Wall -Wextra -pedantic -Werror compilation (#214)
    - minor code-style fixes;

--- hidtest/test.c
+++ hidtest/test.c
@@ -7,7 +7,7 @@
  8/22/2009
 
  Copyright 2009
- 
+
  This contents of this file may be used by anyone
  for any reason without any conditions and may be
  used as a starting point for your own applications
@@ -29,17 +29,15 @@
 
 int main(int argc, char* argv[])
 {
+	(void)argc;
+	(void)argv;
+
 	int res;
 	unsigned char buf[256];
 	#define MAX_STR 255
 	wchar_t wstr[MAX_STR];
 	hid_device *handle;
 	int i;
-
-#ifdef WIN32
-	UNREFERENCED_PARAMETER(argc);
-	UNREFERENCED_PARAMETER(argv);
-#endif
 
 	struct hid_device_info *devs, *cur_dev;
 
@@ -55,7 +53,7 @@ int main(int argc, char* argv[])
 		return -1;
 
 	devs = hid_enumerate(0x0, 0x0);
-	cur_dev = devs;	
+	cur_dev = devs;
 	while (cur_dev) {
 		printf("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls", cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
 		printf("\n");
@@ -73,7 +71,7 @@ int main(int argc, char* argv[])
 	memset(buf,0x00,sizeof(buf));
 	buf[0] = 0x01;
 	buf[1] = 0x81;
-	
+
 
 	// Open the device using the VID, PID,
 	// and optionally the Serial number.
@@ -115,7 +113,7 @@ int main(int argc, char* argv[])
 
 	// Set the hid_read() function to be non-blocking.
 	hid_set_nonblocking(handle, 1);
-	
+
 	// Try to read from the device. There should be no
 	// data here, but execution should not block.
 	res = hid_read(handle, buf, 17);
@@ -158,7 +156,7 @@ int main(int argc, char* argv[])
 		printf("Unable to write()\n");
 		printf("Error: %ls\n", hid_error(handle));
 	}
-	
+
 
 	// Request state (cmd 0x81). The first byte is the report number (0x1).
 	buf[0] = 0x1;
