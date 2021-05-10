053ca2891ab74b05cddafa6dfeed579751f0ee95
Luca Weiss <luca@z3ntu.xyz>
2021-05-10 11:46:18 +0200

    Update hid.c

--- linux/hid.c
+++ linux/hid.c
@@ -45,7 +45,7 @@
 
 #include "hidapi.h"
 
-#error "test CI blabla blub"
+//#error "test CI blabla blub"
 
 #ifdef HIDAPI_ALLOW_BUILD_WORKAROUND_KERNEL_2_6_39
 /* This definitions first appeared in Linux Kernel 2.6.39 in linux/hidraw.h.
