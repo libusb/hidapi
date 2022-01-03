3139e4bb02e67a1f6559153cd6f171ab4fd19a07
Ihor Dutchak <ihor.youw@gmail.com>
2022-01-03 21:01:50 +0200

    Update hidtest/test.c

--- hidtest/test.c
+++ hidtest/test.c
@@ -71,8 +71,7 @@ int main(int argc, char* argv[])
 			printf("  Report Descriptor: ");
 			res = hid_get_report_descriptor(device, buf, sizeof(buf));
 			if (res < 0) {
-				printf("error getting.");
-				printf("%ls", hid_error(device));
+				printf("error getting: %ls", hid_error(device));
 			}
 			else {
 				printf("(%d bytes)", res);
