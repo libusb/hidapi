5f66a371cad20f1e948b2d20d07509084e4d9d72
Megamouse <studienricky89@googlemail.com>
2022-01-29 17:32:25 +0100

    Win32: Don't pass invalid pointers to HidD_FreePreparsedData in hid_open_path (#373)
    Fixes a segfault that happens when hid_open_path is called on a previously disconnected device.
    This is a regression caused by:
    b600727 - Win32: Fix memory leak in `free_hid_device` (#361)
    
    Co-authored-by: Megamouse <studienricky89@googlemail.de>

--- windows/hid.c
+++ windows/hid.c
@@ -815,7 +815,10 @@ HID_API_EXPORT hid_device * HID_API_CALL hid_open_path(const char *path)
 end_of_function:
 	free(interface_path);
 	CloseHandle(device_handle);
-	HidD_FreePreparsedData(pp_data);
+
+	if (pp_data) {
+		HidD_FreePreparsedData(pp_data);
+	}
 
 	return dev;
 }
