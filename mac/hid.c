52c728acf42b4cd86837074cb9d7cf646adc0d85
Ihor Dutchak <ihor.youw@gmail.com>
2022-05-04 14:11:51 +0300

    Use propper type for mach_port_t default param
    - enable compilation warnings as errors for CI to catch such issues in the future;
    - fix Mingw strict compilation warnings;

--- mac/hid.c
+++ mac/hid.c
@@ -790,12 +790,12 @@ static io_registry_entry_t hid_open_service_registry_from_path(const char *path)
 		char *endptr;
 		uint64_t entry_id = strtoull(path + 10, &endptr, 10);
 		if (*endptr == '\0') {
-			return IOServiceGetMatchingService(NULL, IORegistryEntryIDMatching(entry_id));
+			return IOServiceGetMatchingService((mach_port_t) 0, IORegistryEntryIDMatching(entry_id));
 		}
 	}
 	else {
 		/* Fallback to older format of the path */
-		return IORegistryEntryFromPath(NULL, path);
+		return IORegistryEntryFromPath((mach_port_t) 0, path);
 	}
 
 	return MACH_PORT_NULL;
