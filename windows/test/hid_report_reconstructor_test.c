5626729a73193d360914bc7d204093a703ac0850
Ihor Dutchak <ihor.youw@gmail.com>
2022-10-18 01:53:39 +0300

    Fail CTest when hid report reconstruct doesn't match
    Co-authored-by: JoergAtGithub <64457745+JoergAtGithub@users.noreply.github.com>

--- windows/test/hid_report_reconstructor_test.c
+++ windows/test/hid_report_reconstructor_test.c
@@ -539,10 +539,12 @@ int main(int argc, char* argv[])
 			goto end;
 		}
 		else {
+			result = EXIT_FAILURE;
 			fprintf(stderr, "Reconstructed Report Descriptor has different content than expected\n");
 		}
 	}
 	else {
+		result = EXIT_FAILURE;
 		fprintf(stderr, "Reconstructed Report Descriptor has different size: %zu when expected %zu\n", report_descriptor_size, expected_report_descriptor_size);
 	}
 
