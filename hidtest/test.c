1cbbf11c467f64fc16cc4f080c58c538e6baff66
Dmitry Kychanov <k1-801@mail.ru>
2024-04-13 21:34:44 +0400

    Fix more typos

--- hidtest/test.c
+++ hidtest/test.c
@@ -66,9 +66,13 @@ int waitkey()
 #else
     struct termios oldt, newt;
     int ch;
-    int oldf;
 
     tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
 
     ch = getchar();
     while(EOF == ch)
