f6996c91f9ed44f143da078dc91fd9bb153e545b
Youw <Youw@users.noreply.github.com>
2024-10-03 19:25:01 +0000

    deploy: 750bf201ae906eec1dc52c97087c8d0104c091ae

---
+++ search/classes_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['hid_5fapi_5fversion_0',['hid_api_version',['../structhid__api__version.html',1,'']]],
+  ['hid_5fdevice_5finfo_1',['hid_device_info',['../structhid__device__info.html',1,'']]]
+];
