3f5805ea1dfd510a1475a2bf274add12f3e3a69b
Youw <Youw@users.noreply.github.com>
2025-03-13 11:11:59 +0000

    deploy: 0ab6c14264ec76e9328a8eedb3b72b5d27dffd47

---
+++ search/enumvalues_0.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['hid_5fapi_5fbus_5fbluetooth_0',['HID_API_BUS_BLUETOOTH',['../group__API.html#gga15cb543842973ca41daa21feaec534cead412c6f0da75b302b713af3a6394a0d4',1,'hidapi.h']]],
+  ['hid_5fapi_5fbus_5fi2c_1',['HID_API_BUS_I2C',['../group__API.html#gga15cb543842973ca41daa21feaec534ceafdf48944998bb760c6995402eea306a0',1,'hidapi.h']]],
+  ['hid_5fapi_5fbus_5fspi_2',['HID_API_BUS_SPI',['../group__API.html#gga15cb543842973ca41daa21feaec534cea092944e74cc9fe695c08f9e149dcbb38',1,'hidapi.h']]],
+  ['hid_5fapi_5fbus_5funknown_3',['HID_API_BUS_UNKNOWN',['../group__API.html#gga15cb543842973ca41daa21feaec534ceacab9ef7570aa48c13a99b28c61ae918e',1,'hidapi.h']]],
+  ['hid_5fapi_5fbus_5fusb_4',['HID_API_BUS_USB',['../group__API.html#gga15cb543842973ca41daa21feaec534cea77e86719dfaeabc96759f6cff3a5ca8d',1,'hidapi.h']]]
+];
