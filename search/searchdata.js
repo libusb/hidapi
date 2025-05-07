7d7cbf4065cf4ce609d65367d4443c1b39a4caea
Youw <Youw@users.noreply.github.com>
2025-05-07 08:26:45 +0000

    deploy: 3425db0feefeb20d12611b369d6bddcb8b993b07

---
+++ search/searchdata.js
@@ -0,0 +1,45 @@
+var indexSectionsWithContent =
+{
+  0: "abdhimnoprsuv",
+  1: "h",
+  2: "h",
+  3: "h",
+  4: "bimnprsuv",
+  5: "h",
+  6: "h",
+  7: "h",
+  8: "h",
+  9: "ah",
+  10: "dho"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "enums",
+  7: "enumvalues",
+  8: "defines",
+  9: "groups",
+  10: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Enumerations",
+  7: "Enumerator",
+  8: "Macros",
+  9: "Modules",
+  10: "Pages"
+};
+
