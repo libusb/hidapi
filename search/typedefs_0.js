a21da5058e35041c4d16bc1fcb486f5d3510f859
Youw <Youw@users.noreply.github.com>
2025-11-25 12:56:30 +0000

    deploy: a7587175f35ddcbdfb1ce5a1e4dd7a268699ba7f

---
+++ search/typedefs_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hid_5fdevice_0',['hid_device',['../hidapi_8h.html#aa6da74d5686d198dd3e5440e60088fcc',1,'hidapi.h']]]
+];
---
+++ search/variables_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['next_0',['next',['../structhid__device__info.html#a2bfebc240baf3bdaf03965816e11f149',1,'hid_device_info']]]
+];
