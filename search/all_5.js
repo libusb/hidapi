0ad88a2e6d58b9567a634a76fd10625a89e8bdc0
Youw <Youw@users.noreply.github.com>
2026-05-21 08:12:22 +0000

    deploy: c3509c11174fe80ff59a47119433a7db5299af85

---
+++ search/all_5.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['major_0',['major',['../structhid__api__version.html#a57b73e2eae9c7670239442466d97f2bf',1,'hid_api_version']]],
+  ['manufacturer_5fstring_1',['manufacturer_string',['../structhid__device__info.html#a484eebf746220cd2910954cc861759b7',1,'hid_device_info']]],
+  ['minor_2',['minor',['../structhid__api__version.html#a5ebe257ab40c62b6c85d70811a66bcc7',1,'hid_api_version']]]
+];
---
+++ search/pages_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['output_0',['HIDAPI Doxygen output',['../index.html',1,'']]]
+];
